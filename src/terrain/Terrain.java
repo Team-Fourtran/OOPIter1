d3988b444b7611c9e0890ea59835d403f3430a57
Allison Aguirre <allisonaguirre7106@gmail.com>
2017-02-03 17:09:54 -0500

    Revert "Added ResourcePackage class and Terrain type"
    This reverts commit eee8c689e2c550f83b048efddb95f975bc56ce6d.

--- src/terrain/Terrain.java
+++
@@ -1,9 +0,0 @@
-package terrain;
-
-/*
- * Defines interface for Terrain. Terrain affects the speed at which armies/units can travel
- */
-public interface Terrain {
-	// Returns movement cost for units/armies.
-	public double getMovementCost();
-}
