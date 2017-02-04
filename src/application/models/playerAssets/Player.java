09f085f26afe38c975422a205f75ab955a1dff61
Cameron Jeffords <camj@ufl.edu>
2017-02-04 11:50:01 -0500

    added utility package for tile gen and main method

---
+++ src/application/models/playerAssets/Player.java
@@ -0,0 +1,7 @@
+package application.models.playerAssets;
+
+/**
+ * Created by Cameron on 2/3/2017.
+ */
+public class Player {
+}
---
+++ src/application/models/utility/Subscriber.java
@@ -0,0 +1,4 @@
+package application.models.utility;
+
+public interface Subscriber {
+}
