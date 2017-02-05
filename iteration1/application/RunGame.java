c11e43b2a4d8a522f35d51f374e65e795aaf27a3
Juan Jauregui <juan@jauregui.co>
2017-02-05 13:12:02 -0500

    Added a little to RunGame.java

--- iteration1/application/RunGame.java
+++ iteration1/application/RunGame.java
@@ -1,11 +1,22 @@
 package application;
 
+/* Standard library imports */
+import java.util.ArrayList;
+
+/* Main application packages */
 import application.controllers.*;
-import application.models.*;
+import application.models.playerAsset.Player;
+import application.models.tileState.Map;
 import application.views.*;
 
 /* Named as per Dave's specifications */
 public class RunGame {
-    Controller gameController = new Controller();
-    //Map gameMap = new Map;
+    public static void main(String[] args){
+        Controller gameController = new Controller();
+        View mainView = new View();
+
+        ArrayList<Player> players = new ArrayList<Player>();
+        Player currentPlayer;
+        //Map gameMap = new Map();
+    }
 }
