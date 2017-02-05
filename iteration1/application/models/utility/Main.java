758063b1fa6b286da2574eeebfe669182f42e6db
Cameron Jeffords <camj@ufl.edu>
2017-02-05 17:03:57 -0500

    AStar pathfinding complete! I hope...

--- iteration1/application/models/utility/Main.java
+++ iteration1/application/models/utility/Main.java
@@ -17,7 +17,12 @@ public class Main {
         Map m = new Map(T.execute(), length, width);
         m.printOut();
 
-        Player p = new Player();
+        AStarPathfinder pathfinder = new AStarPathfinder(
+                m.getTileState("T8"),
+                m.getTileState("T11")
+        );
+
+        System.out.println(pathfinder.execute());
 
 //        CommandGenerator cGen = new CommandGenerator(p, m);
 //        cGen.generateCommand("CS_U001");
@@ -26,33 +31,33 @@ public class Main {
 //            c.execute();
 //        }
         
-        CommandGenerator cGen = new CommandGenerator(p, m);   
-        
-        // Adding unit to tile T0
-        Command c = cGen.generateCommand("IU_0_T0_colonist").get(0);
-        c.execute();
-        
-        Command c1 = cGen.generateCommand("IU_0_T0_colonist").get(0);
-        c.execute();
-        
-        java.util.Map<String, TileState> t = m.getTiles();
-        
-        // Testing for tile properties, including Units moving
-        t.get("T0").getTileInfo().addAoEs(new DamageAoE(), new HealingAoE());
-        t.get("T0").getTileInfo().addItems(new OneShotItem(), new Decal("crossBones"));
-        System.out.println(t.get("T0").getProperties());
-        System.out.println(t.get("T1").getProperties());
-        
-        // Move unit 0 from T0 to T1
-        c = cGen.generateCommand("MD_u0_0").get(0);
-        System.out.println("\nPrevious Contents in T0: " + m.getTileState("T0").getOccupance("u0").getClass().getSimpleName());
-        System.out.println("nPrevious Contents in T1: " + m.getTileState("T1").getOccupance("u0"));
-        c.execute();
-        System.out.println("\nNew Contents in T0: " + m.getTileState("T0").getOccupance("u0"));
-        System.out.println("nNew Contents in T1: " + m.getTileState("T1").getOccupance("u0").getClass().getSimpleName());
-        
-        System.out.println(t.get("T0").getProperties());
-        System.out.println(t.get("T1").getProperties());
+//        CommandGenerator cGen = new CommandGenerator(p, m);
+//
+//        // Adding unit to tile T0
+//        Command c = cGen.generateCommand("IU_0_T0_colonist").get(0);
+//        c.execute();
+//
+//        Command c1 = cGen.generateCommand("IU_0_T0_colonist").get(0);
+//        c.execute();
+//
+//        java.util.Map<String, TileState> t = m.getTiles();
+//
+//        // Testing for tile properties, including Units moving
+//        t.get("T0").getTileInfo().addAoEs(new DamageAoE(), new HealingAoE());
+//        t.get("T0").getTileInfo().addItems(new OneShotItem(), new Decal("crossBones"));
+//        System.out.println(t.get("T0").getProperties());
+//        System.out.println(t.get("T1").getProperties());
+//
+//        // Move unit 0 from T0 to T1
+//        c = cGen.generateCommand("MD_u0_0").get(0);
+//        System.out.println("\nPrevious Contents in T0: " + m.getTileState("T0").getOccupance("u0").getClass().getSimpleName());
+//        System.out.println("nPrevious Contents in T1: " + m.getTileState("T1").getOccupance("u0"));
+//        c.execute();
+//        System.out.println("\nNew Contents in T0: " + m.getTileState("T0").getOccupance("u0"));
+//        System.out.println("nNew Contents in T1: " + m.getTileState("T1").getOccupance("u0").getClass().getSimpleName());
+//
+//        System.out.println(t.get("T0").getProperties());
+//        System.out.println(t.get("T1").getProperties());
 
     }
 }
