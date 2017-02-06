247c87c2f0032a766471624fff909d2db5beff19
Juan Jauregui <juan@jauregui.co>
2017-02-06 12:46:25 -0500

    post-nap commit

--- iteration1/application/RunGame.java
+++ iteration1/application/RunGame.java
@@ -1,17 +1,9 @@
 package application;
 
-/* Standard library imports */
-import java.util.ArrayList;
-
-/* Main application packages */
-import application.controllers.*;
-import application.models.playerAsset.Player;
-import application.models.tileState.Map;
-import application.views.MainScreen;
-
 /* Named as per Dave's specifications */
 public class RunGame {
     public static void main(String[] args){
+        //Just initializes a new Game
     	Game game = new Game();
     }
 }
